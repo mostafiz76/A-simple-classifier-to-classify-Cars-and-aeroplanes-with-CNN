f0569d2a49a64af18ead19c18265881b90d83cdb
mostafiz76 <44407241+mostafiz76@users.noreply.github.com>
2019-10-10 10:14:32 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+import time
+
+start = time.time()
+
+#Define Path
+model_path = './models/model.h5'
+model_weights_path = './models/weights.h5'
+test_path = './v_data/train/planes'
+
+#Load the pre-trained models
+model = load_model(model_path)
+model.load_weights(model_weights_path)
+
+#Define image parameters
+img_width, img_height = 224, 224
+
+#Prediction Function
+def predict(file):
+    x = load_img(file, target_size=(img_width,img_height))
+    x = img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+    array = model.predict(x)
+    result = array[0][0]
+    print(array[0][0])
+
+    #answer = np.argmax(array)
+    #print(answer)
+    if result >=0.5:
+        print("Predicted: plane")
+    else:
+        print("Predicted: car")
+    #elif answer == 2:
+    #    print("Predicted: soccer_ball")
+    
+  #return answer
+
+#Walk the directory for every image
+for i, ret in enumerate(os.walk(test_path)):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+    
+    print(ret[0] + '/' + filename)
+    result = predict(ret[0] + '/' + filename)
+    print(" ")
+
+#Calculate execution time
+end = time.time()
+dur = end-start
+
+if dur<60:
+    print("Execution Time:",dur,"seconds")
+elif dur>60 and dur<3600:
+    dur=dur/60
+    print("Execution Time:",dur,"minutes")
+else:
+    dur=dur/(60*60)
+    print("Execution Time:",dur,"hours")
